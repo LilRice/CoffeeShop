315709a6788b55d0ce7586b6c7f9526d573934cb
Bat-Erdene Ganbat <bat@C02SV3D4G8WL.local>
2019-03-28 14:58:35 +0100

    Kodar första sidan. Bilderna är infogade men funktioner saknas

--- index.php
+++ index.php
@@ -8,14 +8,29 @@
 </head>
 <body>
     <div class="container">
+        <header> 
+        <img src="./images/coffee-house-cafe-logo-1.png" id="logo">
     <nav>
     <a href="#">Home</a>
     <a href="#">Coffee Shop</a>
     <a href="#">About</a>
     <a href="#">Contact</a>
     </nav>
-    
-    
+   </header>
+   <img src="./images/How-Can-You-Possibly-Perform-At-Your-Peak.png" class="slider">
+   <button class="yellowbutton">Search for coffee shop</button>
+
+   <div class="imagenav">
+    <img src="./images/Bellagio-Patisserie-coffee.png" alt="">
+    <img src="./images/coffeeshop.png" alt="">
+    <img src="./images/download.png" alt="">
+    <img src="./images/revolver-2-600x300.png" alt="">
+
+    <button class="imagebutton-car">Careers</button>
+    <button class="imagebutton-about">About Us</button>
+    <button class="imagebutton-shop">Coffee Shops</button>
+    <button class="imagebutton-con">Contact Us</button>
+        </div>
     </div>
 </body>
 </html>
